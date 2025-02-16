902c8b4157b04871503101cc09f81bddf6811d49
akshitdsa <akshit123on@gmail.com>
2025-02-16 11:48:45 +0530

    Time: 0 ms (100%), Space: 10.7 MB (96.77%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,4 +1,10 @@
 //Using Set
+// Why choose this approach?
+//We only need to count unique positive numbers, which is exactly what a set is built for.
+//Using an unordered_set, inserting elements is O(1) on average.
+//Thus, the entire process runs in O(n) time.
+
+
 //TC:O(n) to insert elements into the set.
 //O(1) lookup for set operations.
 //Total Complexity: O(n) 
@@ -13,10 +19,14 @@
 //     }
 // };
 
+// Why choose this approach?
+//Sorting groups duplicate numbers together, making it easy to count unique positive numbers.
+//It is useful if set operations are costly (e.g., in constrained environments).
 //Using Sorting (Alternative)
 //TC:Sorting → O(n log n)
 //Traversal → O(n)
-//Total Complexity: O(n log n) 
+//Total Complexity: O(n log n)
+ 
 class Solution {
 public:
 int minimumOperations(vector<int>& nums) {

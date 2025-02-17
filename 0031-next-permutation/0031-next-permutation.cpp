5741ad8eea7428e5d5bb992bd506db68a34de2c3
akshitdsa <akshit123on@gmail.com>
2025-02-17 12:25:05 +0530

    Time: 0 ms (100%), Space: 15.6 MB (67.56%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        //first blue gola indx pakdo
+        int gola_idx = -1;
+        for(int i =n-1; i>0;i--)
+        {
+            if(nums[i]>nums[i-1])
+            {
+                gola_idx = i-1;
+                break;
+            }
+        }
+        if(gola_idx !=-1)
+        {
+            int swap_idx = gola_idx;
+            for(int j = n-1;j>=gola_idx+1;j--)
+            {
+                if(nums[j]>nums[gola_idx])
+                {
+                    swap_idx = j;
+                    break;
+                }
+            }
+            swap(nums[gola_idx],nums[swap_idx]);
+        }
+        reverse(nums.begin()+gola_idx+1,nums.end());
+    }
+};

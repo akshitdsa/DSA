1ca1e41fa05bff8c76cb36b5b4f0fc1e648c64f6
akshitdsa <akshit123on@gmail.com>
2025-02-18 10:01:19 +0530

    Time: 4 ms (14.62%), Space: 77.3 MB (25.58%) - LeetHub

--- 2684-determine-the-winner-of-a-bowling-game/2684-determine-the-winner-of-a-bowling-game.cpp
+++ 2684-determine-the-winner-of-a-bowling-game/2684-determine-the-winner-of-a-bowling-game.cpp
@@ -1,57 +1,57 @@
 //Optimezed Approach
 //TC:O(n);
 //SC:O(1);
-class Solution {
-public:
-    int isWinner(vector<int>& player1, vector<int>& player2) {
-    int score1 = calculateScore(player1);
-    int score2 = calculateScore(player2);
-
-    return score1==score2 ?0 : (score1>score2?1:2);
-    }
-
-    int calculateScore(vector<int>& player) {
-    int n = player.size();
-    int score = 0;
-    int last10Index =-1;
-    for (int i = 0; i < n; i++) {
-        if(last10Index !=-1 && i-last10Index <=2)
-        {
-            score +=player[i]+player[i];
-        }
-        else
-        {
-            score+=player[i];
-        }
-        if(player[i]==10)
-        {
-            last10Index =i;
-        }
-    }
-    return score;
-}
-};
-
-//BruteForce Approach
-//TC:O(n);
-//SC:O(1);
 // class Solution {
 // public:
 //     int isWinner(vector<int>& player1, vector<int>& player2) {
 //     int score1 = calculateScore(player1);
 //     int score2 = calculateScore(player2);
 
-//     return score1==score2 ? 0 : (score1>score2?1:2);
+//     return score1==score2 ?0 : (score1>score2?1:2);
 //     }
-    
+
 //     int calculateScore(vector<int>& player) {
 //     int n = player.size();
 //     int score = 0;
+//     int last10Index =-1;
 //     for (int i = 0; i < n; i++) {
-//        bool doublePoints = (player[i-1]==10 ||player[i-2]==10);
-//        score+ = doublePoints ? 2*player[i]:player[i];
+//         if(last10Index !=-1 && i-last10Index <=2)
+//         {
+//             score +=player[i]+player[i];
+//         }
+//         else
+//         {
+//             score+=player[i];
+//         }
+//         if(player[i]==10)
+//         {
+//             last10Index =i;
+//         }
 //     }
 //     return score;
 // }
 // };
+
+//BruteForce Approach
+//TC:O(n);
+//SC:O(1);
+class Solution {
+public:
+    int isWinner(vector<int>& player1, vector<int>& player2) {
+    int score1 = calculateScore(player1);
+    int score2 = calculateScore(player2);
+
+    return score1 == score2 ? 0 : (score1>score2?1:2);
+    }
+    
+    int calculateScore(vector<int>& player) {
+    int n = player.size();
+    int score = 0;
+    for (int i = 0; i < n; i++) {
+       bool doublePoints = (i>0 && player[i-1]==10 || i>1 && player[i-2]==10);
+       score  += (doublePoints ? 2*player[i]:player[i]);
+    }
+    return score;
+}
+};
 

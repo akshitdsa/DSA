91c096d1476b2b37d73dc4da27ae8d7b5ce6e6e8
akshitdsa <akshit123on@gmail.com>
2025-02-19 09:29:07 +0530

    Time: 0 ms (100%), Space: 23.9 MB (18.09%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -18,14 +18,14 @@ public:
         int n = nums.size();
         int i=0;
         int j=0;
-        while(i<n)
+        while(j<n)
         {
-            if(nums[i]!=0)
+            if(nums[j]!=0)
             {
                 swap(nums[i],nums[j]);
-                j++;
+                i++;
             }
-            i++;
+            j++;
         }
       }
 };

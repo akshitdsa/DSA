c75a6e11e50d0bce9fed93a03eda90da4af3bb24
akshitdsa <akshit123on@gmail.com>
2025-02-24 19:08:44 +0530

    Time: 6 ms (70.26%), Space: 11.6 MB (81.24%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -20,25 +20,49 @@
 
 //Sliding Window using set
 //TC:O(N)
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-          int left = 0;
-          int right =0;
-        int maxLength = 0;
-        unordered_set<char> charSet;
-        while (right<s.length()) {
-            while (charSet.find(s[right]) != charSet.end()) {
-                charSet.erase(s[left]);
-                left++;
-            }
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//           int left = 0;
+//           int right =0;
+//         int maxLength = 0;
+//         unordered_set<char> charSet;
+//         while (right<s.length()) {
+//             while (charSet.find(s[right]) != charSet.end()) {
+//                 charSet.erase(s[left]);
+//                 left++;
+//             }
 
-            charSet.insert(s[right]);
-            maxLength = max(maxLength, right - left + 1);
-            right++;
-        }
+//             charSet.insert(s[right]);
+//             maxLength = max(maxLength, right - left + 1);
+//             right++;
+//         }
         
 
-        return maxLength;    
+//         return maxLength;    
+//     }
+//     };
+
+    //Sliding Window using hashmap
+//TC:O(N)
+class Solution {
+public:
+int lengthOfLongestSubstring(string s) {
+    unordered_map<char, int> count; // Stores character frequency
+    int left = 0, maxLength = 0;
+
+    for (int right = 0; right < s.length(); right++) {
+        count[s[right]]++; // Increase frequency of current character
+
+        while (count[s[right]] > 1) { // If duplicate found
+            count[s[left]]--; // Remove left character
+            left++; // Move left pointer
+        }
+
+        maxLength = max(maxLength, right - left + 1);
     }
-    };
+
+    return maxLength;
+
+}
+};
